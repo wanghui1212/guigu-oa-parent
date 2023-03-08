dcdd840ae1b8f61ae0ef3f3cd7cecb2f87381fd4
3120002988+王辉 <2417337692@qq.com>
2023-03-08 23:35:06 +0800

    权限管理模块-获取用户信息

--- service-oa/src/main/java/com/atguigu/auth/controller/IndexController.java
+++ service-oa/src/main/java/com/atguigu/auth/controller/IndexController.java
@@ -1,12 +1,18 @@
 package com.atguigu.auth.controller;
 
+import com.atguigu.auth.service.SysUserService;
+import com.atguigu.common.config.exception.GuiguException;
+import com.atguigu.common.jwt.JwtHelper;
 import com.atguigu.common.result.Result;
+import com.atguigu.common.utils.MD5;
+import com.atguigu.model.system.SysUser;
+import com.atguigu.vo.system.LoginVo;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,21 +27,59 @@ import java.util.Map;
 @RequestMapping("/admin/system/index")
 public class IndexController {
     //login
-    @PostMapping("login")
+    /*@PostMapping("login")
     public Result login() {
         Map<String, Object> map = new HashMap<>();
         map.put("token", "admin-token");
         return Result.ok(map);
+    }*/
+    @Autowired
+    private SysUserService sysUserService;
+
+    @ApiOperation(value = "登录")
+    @PostMapping("login")
+    public Result login(@RequestBody LoginVo loginVo) {
+        SysUser sysUser = sysUserService.getByUsername(loginVo.getUsername());
+        if(null == sysUser) {
+            throw new GuiguException(201,"用户不存在");
+        }
+        if(!MD5.encrypt(loginVo.getPassword()).equals(sysUser.getPassword())) {
+            throw new GuiguException(201,"密码错误");
+        }
+        if(sysUser.getStatus().intValue() == 0) {
+            throw new GuiguException(201,"用户被禁用");
+        }
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("token", JwtHelper.createToken(sysUser.getId(), sysUser.getUsername()));
+        return Result.ok(map);
     }
 
     //info
-    @GetMapping("info")
+   /* @GetMapping("info")
     public Result info() {
         Map<String, Object> map = new HashMap<>();
-        map.put("roles", "['admin']");
-        map.put("introduction", "I am a super administrator");
-        map.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
-        map.put("name", "super admin");
+        map.put("roles", "[admin]");
+        map.put("name","admin");
+        map.put("avatar","https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+        //返回菜单
+        map.put("routers", new ArrayList<>());
+        //返回按钮
+        map.put("buttons", new ArrayList<>());
+        return Result.ok(map);
+    }*/
+    @ApiOperation(value = "获取用户信息")
+    @GetMapping("info")
+    public Result info(HttpServletRequest request) {
+        /*String username = JwtHelper.getUsername(request.getHeader("token"));
+        System.out.println("username:"+username);
+        Map<String, Object> map = sysUserService.getUserInfo(username);*/
+        String token = request.getHeader("token");
+        System.out.println("token = " + token);
+        String username = JwtHelper.getUsername(token);
+        System.out.println("username = " + username);
+        Map<String, Object> map = sysUserService.getUserInfo(username);
+
         return Result.ok(map);
     }
     //logout
--- service-oa/src/main/java/com/atguigu/auth/service/SysMenuService.java
+++ service-oa/src/main/java/com/atguigu/auth/service/SysMenuService.java
@@ -2,6 +2,7 @@ package com.atguigu.auth.service;
 
 import com.atguigu.model.system.SysMenu;
 import com.atguigu.vo.system.AssginMenuVo;
+import com.atguigu.vo.system.RouterVo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 import java.util.List;
@@ -33,4 +34,16 @@ public interface SysMenuService extends IService<SysMenu> {
      * @param assginMenuVo
      */
     void doAssign(AssginMenuVo assginMenuVo);
+    /**
+     * 获取用户菜单
+     * @param userId
+     * @return
+     */
+    List<RouterVo> findUserMenuList(Long userId);
+    /**
+     * 获取用户按钮权限
+     * @param userId
+     * @return
+     */
+    List<String> findUserPermsList(Long userId);
 }
